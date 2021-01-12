73363014738cfb60fd33c589acbbc500161705c3
Khalid Saifullah <khalid@outlook.com.au>
2021-01-13 08:15:35 +1100

    clean up

--- client/src/services/server-to-s3.ts
+++ client/src/services/server-to-s3.ts
@@ -6,14 +6,14 @@ import { downloadFromSftpServer } from './download-from-sftp-server';
 import { uploadToS3 } from './upload-to-s3';
 
 export const serverToS3 = async (options: ServerToS3Options): Promise<void> => {
-  const storageLocation = process.env['STORAGE_LOCATION'] || '/tmp/download/';
+  const storageLocation = process.env['STORAGE_LOCATION'] || '/tmp/';
   fs.mkdirSync(storageLocation, {recursive: true});
   const localDir = fs.mkdtempSync(storageLocation);
   const localPaths = await downloadFromSftpServer(options, localDir);
 
   const {bucket, keyPrefix} = options;
-  const keyPrefixWithDate = moment().tz('utc').format(keyPrefix);
+  const normalisedKeyPrefix = moment().tz('utc').format(keyPrefix);
   for (const localPath of localPaths) {
-    await uploadToS3(localPath, bucket, `${keyPrefixWithDate}${path.basename(localPath)}`);
+    await uploadToS3(localPath, bucket, `${normalisedKeyPrefix}${path.basename(localPath)}`);
   }
 };
