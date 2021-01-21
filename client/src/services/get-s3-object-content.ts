10ca2c9e826499c71be45e3356a5200ee2375720
Khalid Saifullah <khalid@outlook.com.au>
2021-01-21 12:28:20 +1100

    clean up

--- client/src/services/get-s3-object-content.ts
+++ client/src/services/get-s3-object-content.ts
@@ -3,7 +3,6 @@ import { parseS3Uri } from './parse-s3-uri';
 
 export const getS3ObjectContent = async (uri: string): Promise<string> => {
   const {bucket, key} = parseS3Uri(uri);
-  console.log(bucket,key);
 
   const s3 = new S3({apiVersion: '2006-03-01'});
   const data = await s3.getObject({
